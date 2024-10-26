76325918e5a66152d16a784a0c455dbee7a45c60
sufyanilyas06 <sufyanilyas66@gmail.com>
2024-10-27 00:49:19 +0500

    Update activities.c

--- activities.c
+++ activities.c
@@ -7,27 +7,40 @@ void main()
 } */
 //-----------------------------------
 // ACTIVITY 2.3
-#include<stdio.h>
-void main()
-{
-    int rollNo;
-    float percentage;
-    char grade;
-    printf("enter you roll # :");
-    scanf("%d",&rollNo);
-    printf("enter your percentage :");
-    scanf("%f",&percentage);
-    printf("enter your grade :");
-    /*there is problem here is that when we use scanf to take 
-     charater form user it will not be able to do this, because  when you 
-     press enter after storing value in the percentage the enter pressed by
-     you is stored in the charater so we can resolve this error by adding space 
-     at the start of %c  as I mentioned below  */
-    scanf(" %c",&grade);
-    printf("Roll NO\t: %d\n",rollNo);
-    printf("Percentage\t: %f\n",percentage);
-    printf("Grade\t : %c\n",grade);
-}
+// #include<stdio.h>
+// void main()
+// {
+//     int rollNo;
+//     float percentage;
+//     char grade;
+//     printf("enter you roll # :");
+//     scanf("%d",&rollNo);
+//     printf("enter your percentage :");
+//     scanf("%f",&percentage);
+//     printf("enter your grade :");
+//     /*there is problem here is that when we use scanf to take 
+//      charater form user it will not be able to do this, because  when you 
+//      press enter after storing value in the percentage the enter pressed by
+//      you is stored in the charater so we can resolve this error by adding space 
+//      at the start of %c  as I mentioned below  */
+//     scanf(" %c",&grade);
+//     printf("Roll NO\t: %d\n",rollNo);
+//     printf("Percentage\t: %f\n",percentage);
+//     printf("Grade\t : %c\n",grade);
+// }
+//-----------------------------------
+// ACTIVITY 2.4
+// #include<stdio.h>
+// void main()
+// {
+    
+//     float length;
+    
+//     printf("enter length of square :");
+//     scanf("%f",&length);
+//     printf("Area of square is : %.2f ",length*length);
 
-
+// }
+//-----------------------------------
+// ACTIVITY 2.4
 
