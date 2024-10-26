84880f275ba51176f78e1c05e8f937ca105dd4ab
sufyanilyas06 <sufyanilyas66@gmail.com>
2024-10-27 00:16:58 +0500

    Update activities.c

--- activities.c
+++ activities.c
@@ -1,5 +1,33 @@
-#include<iostream>
+// ---------- these are the activities of chapter # 2 (user interface)------------
+// ACTIVITY 2.2:
+/* #include<iostream>
 void main()
 {
     printf("SUFYAN ilyas");
+} */
+//-----------------------------------
+// ACTIVITY 2.3
+#include<stdio.h>
+void main()
+{
+    int rollNo;
+    float percentage;
+    char grade;
+    printf("enter you roll # :");
+    scanf("%d",&rollNo);
+    printf("enter your percentage :");
+    scanf("%f",&percentage);
+    printf("enter your grade :");
+    /*there is problem here is that when we use scanf to take 
+     charater form user it will not be able to do this, because  when you 
+     press enter after storing value in the percentage the enter pressed by
+     you is stored in the charater so we can resolve this error by adding space 
+     at the start of %c  as I mentioned below  */
+    scanf(" %c",&grade);
+    printf("Roll NO\t: %d\n",rollNo);
+    printf("Percentage\t: %f\n",percentage);
+    printf("Grade\t : %c\n",grade);
 }
+
+
+
